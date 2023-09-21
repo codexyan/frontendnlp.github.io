2599bd1a20d1e89af95fab6499a195a42495bbc6
codexyan <mdcodeid@gmail.com>
2023-09-21 15:39:01 +0700

    update slider and cta-buy

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// Banner Native Slide
+const slider = document.querySelector(".slider")
+const bannerImages = document.querySelectorAll(".banner-img")
+const circles = document.querySelectorAll(".circle")
+let currentIndex = 0
+
+function updateSlider() {
+    bannerImages.forEach((img, index) => {
+        img.style.transform = `translateX(-${currentIndex * 100}%)`
+        circles[index].classList.remove("circle-actived")
+    })
+
+    circles[currentIndex].classList.add("circle-actived")
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % bannerImages.length
+    updateSlider()
+}
+
+setInterval(nextSlide, 6000)
+
+// CTA product
+const ctaBuy = document.querySelector("#cta-pesan")
+ctaBuy.addEventListener("click", function () {
+    
+})
