87c76feb5076158edc99761bc7ca38a8477d669f
codexyan <mdcodeid@gmail.com>
2023-09-26 23:35:32 +0700

    update counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Banner Native Slide
-  const slider = document.querySelector(".slider");
+  // Slider banner activation
   const bannerImages = document.querySelectorAll(".banner-img");
   const circles = document.querySelectorAll(".circle");
   let currentIndex = 0;
@@ -22,42 +21,28 @@ document.addEventListener("DOMContentLoaded", function () {
   setInterval(nextSlide, 6000);
 
   // CTA product
-  const heroBanner = document.querySelector(".hero-container");
-  const notification = document.querySelector(".notification");
-  const category = document.querySelector(".category");
-  const menu = document.querySelector(".menu");
-  const blurElements = [heroBanner, notification, category, menu];
+  const blurElements = document.querySelectorAll(
+    ".hero-container, .notification, .category, .menu"
+  );
 
-  const modalOverlays = document.querySelectorAll(".modal-overlay");
   const ctaBuyButtons = document.querySelectorAll(".cta-pesan");
   const ctaCloseButtons = document.querySelectorAll(".cta-back");
 
-  // Agar checkbox hanya dapat memilih salah satu saja
   ctaBuyButtons.forEach((ctaBuyButton) => {
     ctaBuyButton.addEventListener("click", function () {
-      // Mengambil ID modal yang sesuai dari atribut "data-modal"
       const modalId = this.getAttribute("data-modal");
       const modalOverlay = document.getElementById(modalId);
 
-      // Memeriksa apakah modal ditemukan
       if (modalOverlay) {
-        // Menampilkan modal dan efek lainnya
         modalOverlay.style.display = "flex";
         modalOverlay.style.transform = "translateY(0%)";
         modalOverlay.style.boxShadow = "5px 5px 12px rgba(0, 0, 0, 0.3)";
-
-        // Menampilkan modal dengan mengaktifkan kelas "show"
         modalOverlay.classList.add("show");
 
-        // Menambahkan efek blur ke elemen-elemen yang ditentukan
         blurElements.forEach((element) => {
           element.classList.add("blur");
         });
 
-        // Menyimpan referensi ke modal yang sedang aktif
-        activeModal = modalOverlay;
-
-        // Agar checkbox hanya dapat memilih salah satu saja di modal yang sedang aktif
         const checkboxes = modalOverlay.querySelectorAll(".custom-checkbox");
         checkboxes.forEach(function (checkbox) {
           checkbox.addEventListener("change", function () {
@@ -72,31 +57,24 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  // Menambahkan event listener pada setiap tombol "Tutup"
   ctaCloseButtons.forEach((ctaCloseButton) => {
     ctaCloseButton.addEventListener("click", function () {
-      // Mengambil elemen modal yang menjadi parent dari tombol "Tutup"
       const modalOverlay = this.closest(".modal-overlay");
 
-      // Memeriksa apakah modal ditemukan
       if (modalOverlay) {
-        // Menyembunyikan modal dengan efek animasi
         modalOverlay.style.transform = "translateY(100%)";
         modalOverlay.style.boxShadow = "none";
         modalOverlay.classList.remove("show");
 
-        // Tambahkan event listener untuk mendeteksi akhir animasi
         modalOverlay.addEventListener(
           "transitionend",
           function () {
-            // Setelah animasi selesai, sembunyikan modal dan menghapus efek blur
             modalOverlay.style.display = "none";
           },
           { once: true }
-        ); // { once: true } agar event listener ini hanya berjalan satu kali
+        );
       }
 
-      // Menghapus efek blur dari elemen-elemen yang ditentukan
       blurElements.forEach((element) => {
         element.classList.remove("blur");
       });
@@ -104,96 +82,105 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // Counter Pesanan Product
+  let counter = 1;
   const minButtons = document.querySelectorAll(".counter-min");
   const plusButtons = document.querySelectorAll(".counter-add");
-  const counterElements = document.querySelectorAll(".value");
-  let counter = 1;
 
-  function addToCounter() {
-    counter++;
-    // Perlu mencari indeks elemen yang diklik
-    const index = Array.from(plusButtons).indexOf(this);
-
-    if (index !== -1) {
-      // Mengupdate elemen dengan indeks yang sesuai
-      counterValueElements[index].textContent = counter;
-    }
-  }
-
-  function minFromCounter() {
-    if (counter > 0) {
-      counter--;
-      // Perlu mencari indeks elemen yang diklik
-      const index = Array.from(minButtons).indexOf(this);
-
-      if (index !== -1) {
-        // Mengupdate elemen dengan indeks yang sesuai
-        counterValueElements[index].textContent = counter;
-      }
-    }
-  }
-
-  // Menambahkan event listener pada setiap tombol "Tambah"
-plusButtons.forEach((plusButton, index) => {
-  plusButton.addEventListener("click", () => {
-    let value = parseInt(counterValueElements[index].textContent);
-    value++;
-    counterValueElements[index].textContent = value;
-    updateTotalHarga();
-  });
-});
-
-minButtons.forEach((minButton, index) => {
-  minButton.addEventListener("click", () => {
-    let value = parseInt(counterValueElements[index].textContent);
-    if (value > 1) {
-      value--;
-      counterValueElements[index].textContent = value;
-      updateTotalHarga();
-    }
-  });
-});
-
-  // --------------------------------
-  // Hitung Harga
-  // Mendapatkan elemen-elemen yang diperlukan untuk setiap produk
-  const checkboxes = document.querySelectorAll(
-    "#modal-overlay-1 .custom-checkbox"
-  );
-  const counterValueElements = document.querySelectorAll(".value");
-  const feeBoxed = document.querySelector("#modal-overlay-1 .fee-boxed");
-  const hargaElements = document.querySelectorAll(".harga");
-
-  // Menyimpan harga item yang sedang dipilih untuk setiap produk
-  const selectedHarga = Array.from(checkboxes).map((checkbox, index) => {
+  const counterElements = document.querySelectorAll(".counter");
+  const counters = Array.from(counterElements).map((counterElement) => {
+    const index = counterElement.getAttribute("data-index");
+    const valueElement = counterElement.querySelector(".value");
     return {
-      isChecked: false,
-      harga: parseFloat(hargaElements[index].textContent),
+      value: parseInt(valueElement.textContent),
+      valueElement,
+      index,
     };
   });
 
-  // Fungsi untuk mengupdate total harga
-function updateTotalHarga() {
-  let totalHarga = 0;
+  function addToCounter(index) {
+    counters[index].value++;
+    counters[index].valueElement.textContent = counters[index].value;
+    updateTotalHarga();
+  }
 
-  selectedHarga.forEach((item, index) => {
-    if (item.isChecked) {
-      const jumlahPesanan = parseInt(counterValueElements[index].textContent);
-      // Menggunakan counterValueElements
-      totalHarga += item.harga * jumlahPesanan;
-      feeBoxed[index].textContent = `- Rp. ${totalHarga.toFixed(2)}`;
-    } else {
-      feeBoxed[index].textContent = "";
-    }
-  });
-}
-
-
-  // Menambahkan event listener pada setiap checkbox
-  checkboxes.forEach((checkbox, index) => {
-    checkbox.addEventListener("change", () => {
-      selectedHarga[index].isChecked = checkbox.checked;
+  function minFromCounter(index) {
+    if (counters[index].value > 1) {
+      counters[index].value--;
+      counters[index].valueElement.textContent = counters[index].value;
       updateTotalHarga();
-    });
+    }
+  }
+
+  plusButtons.forEach((plusButton, index) => {
+    plusButton.addEventListener("click", () => addToCounter(index));
   });
+
+  minButtons.forEach((minButton, index) => {
+    minButton.addEventListener("click", () => minFromCounter(index));
+  });
+
+  // Hitung Harga Modal Layer 1
+  const feeBoxedElement = document.querySelector(".fee-boxed");
+
+  // Harga
+  const hargaElementsOri1 = document.querySelector(".name-choose:nth-child(1) .harga");
+
+  // debugging
+  if (hargaElementsOri1) {
+    const hargaText = hargaElementsOri1.textContent.trim();
+    const harga = parseInt(hargaText.replace(/\D/g, ""));
+    console.log("Harga dari checkbox1:", harga);
+  } else {
+    console.log("Elemen harga tidak ditemukan.");
+  }
+
+  // Checkbox
+  const checkbox1 = document.getElementById("rounded-checkbox1");
+  const checkbox2 = document.getElementById("rounded-checkbox2");
+
+  let isCheckboxChecked = false;
+
+  function updateTotalHarga() {
+    const hargaText = hargaElementsOri1.textContent.trim();
+    const harga = parseInt(hargaText.replace(/\D/g, ""));
+    const valueElementAtIndex0 = document.querySelector(
+      ".counter[data-index='0'] .value"
+    );
+    const valueAsNumber = parseInt(valueElementAtIndex0.textContent);
+    // Mengambil angka dari teks harga
+
+    let totalHarga = harga * valueAsNumber;
+
+    if (isCheckboxChecked) {
+      console.log(counters);
+      console.log("hargaText:", hargaText);
+      console.log("harga:", harga);
+      console.log("totalHarga:", totalHarga);
+      console.log("berhasil");
+      feeBoxedElement.textContent = `- Rp ${totalHarga}`;
+    } else {
+      feeBoxedElement.textContent = "";
+    }
+  }
+
+  checkbox1.addEventListener("change", function () {
+    isCheckboxChecked = checkbox1.checked;
+    updateTotalHarga();
+  });
+
+  // checkbox2.addEventListener("change", function () {
+  //   // Periksa apakah checkbox2 dipilih
+  //   if (checkbox2.checked) {
+  //     const hargaText = document
+  //       .querySelector(".name-choose:nth-child(2) .harga")
+  //       .textContent.trim();
+  //     const harga = parseInt(hargaText.replace(/\D/g, ""));
+
+  //     // Sekarang Anda memiliki nilai harga yang sesuai dengan checkbox2 yang dipilih
+  //     console.log("Harga dari checkbox2:", harga);
+  //   }
+
+  //   // Panggil fungsi updateTotalHarga() di sini untuk menghitung total harga jika diperlukan
+  //   updateTotalHarga();
+  // });
 });
